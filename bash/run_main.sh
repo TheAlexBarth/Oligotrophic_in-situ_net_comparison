417aae374bae9e710bfe60fa01fc3bdefaf53c41
Alex Barth <AB93@email.sc.edu>
2022-05-09 16:33:50 -0400

    deleted bash

--- bash/run_main.sh
+++
@@ -1,7 +0,0 @@
-cd ~/Documents/GitClones/Oligotrophic_in-situ_net_comparison/R
-Rscript mgmt_01_mocness-formatting.R
-Rscript main_analysis_01_size-distribution.R
-Rscript main_analysis_02_rel-abundance.R
-Rscript mgmt_03_moc_binned-density.R
-Rscript mgmt_03_uvp_binned-density.R
-Rscript main_analysis_03_density-depth-profiles.R
